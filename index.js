5fa78bd02f6ce78a1c8fc3613139c60bce568f73
enti <enti@enti.enti>
2022-02-11 14:27:36 +0100

    Personaje, coches y movimiento de coches añadido

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 let http = require("http");
 let fs=require("fs");
-const data =require('./images.json');
+let archivo =require('./images.json');
 
 let http_server = http.createServer(function(req,res) {
 
-for(let i=0; i < data.images.length; i++){
-	if (req.url == "/" + data.images[i]){
-		fs.readFile(data.images[i], function(err, data) {
+for(let i=0; i < archivo.images.length; i++){
+	if (req.url == "/" + archivo.images[i]){
+		fs.readFile(archivo.images[i], function(err, data) {
 			if (err) {
 				console.log("Error");
 				return
